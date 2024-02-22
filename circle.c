61f9ca65c10bfeee934f8275536617eb75611cba
samojeyinka <samuelojeyinka@gmail.com>
2024-02-21 20:14:09 -1200

    -

--- circle.c
+++ circle.c
@@ -2,11 +2,13 @@
 
 
 int main(){
-    double pie = 3.14;
-    double r;
-
+    double pie = 3.14,r;
+    
     printf("Program to calculate the area and circumference of a circle");
     scanf("%lf", &r);
+
+    // Since area of circle = πr^2
+    // Since Circumference of circle = 2πr
     double area = pie * (r * r );
     double circumference = 2 * pie * r;
     printf("Area of the circle = %f\n", area);
